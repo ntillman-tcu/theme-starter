6356e8274f15090429eba3c67f5adaef971792f0
Nicholas Tillman <n.tillman@tcu.edu>
2016-06-14 14:00:59 -0500

    Initial commit

---
+++ footer.php
@@ -0,0 +1,20 @@
+    <p>&copy; <?php the_date('Y') | bloginfo('name'); ?> all rights reserved</p>
+    <p>Theme Developed by Nicholas Tillman &amp; Lee Thorpe</p>
+	</main>
+	
+	<!-- Footer Scripts - will enqueue these -->
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+  <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
+
+	 <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+  <script>
+    (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+    function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+    e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+    e.src='//www.google-analytics.com/analytics.js';
+    r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+    ga('create','UA-XXXXX-X');ga('send','pageview');
+  </script>
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title><?php bloginfo('name'); ?></title>
+	<meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	
+	<main role="main">
+
+	<header>
+		<div class="branding"></div>
+		<nav>
+		<!-- Exchange for wp_nav_menus() -->
+			<ul>
+				<li><a href=""></a></li>
+				<li><a href=""></a></li>
+				<li><a href=""></a></li>
+				<li><a href=""></a></li>
+			</ul>
+		</nav>
+	</header>
+	<h2>This is the header</h2>
